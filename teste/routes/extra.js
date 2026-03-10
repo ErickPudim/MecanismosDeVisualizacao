5c7ccaec1be138d677814a2b31e40ea16d723d22
Erick de Souza Pereira <ericksouzap56@gmail.com>
2026-03-10 15:11:00 -0300

    HBS feito

--- teste/routes/extra.js
+++ teste/routes/extra.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'Página dos usuários' });
+  res.render('extra', { title: 'Contador de Elefantes' });
 });
 
 module.exports = router;
